cc813d0f5a96ad1edd0515f5665b861495b7061a
Trezeno <113196678+Trezeno@users.noreply.github.com>
2024-03-25 12:08:31 +0000

    Overhaul of wave system
    - Added autoload of path_generator.gd script to be accessible as PathGenInstance
    - Created path_generator_config that extends Resoruce to enable saving/changing of level variables much easier
    - Created resources to store data for our easy/medium/map extending above new config
    - Added few comments of some functions to allow better understanding
    - Complete overhaul of previous map_parent and path_generator code to better suit new system

--- Scenes/Maps/easy_map.gd
+++ Scenes/Maps/easy_map.gd
@@ -3,10 +3,16 @@ extends MapParent
 var enemy_array: Array = ["Scumbug", "Giant Zombie Snail"]
 @onready var next_wave_button: Button = $UI/MarginContainer/HBoxContainer/NextWaveButton
 
+var easy_map_config = preload("res://Resources/easy_map_config.res")
+
+func _init():
+	PathGenInstance.path_config = easy_map_config
+	
 func _ready():
-	_display_path()
+	PathGenInstance.generate_new_path()
 	_complete_grid()
 	
+# Placeholder wave spawner -> Probably be its own global script always available
 func _on_ui_next_wave_button_pressed(wave_number, wave_size):
 	Globals.wave_ongoing = true
 	print("Starting wave: " + str(wave_number))
@@ -18,12 +24,13 @@ func _on_ui_next_wave_button_pressed(wave_number, wave_size):
 		wave_size -= temp_enemy_size
 		await get_tree().create_timer(0.2).timeout
 		
+# Placeholder regeneration of map/path layout after 5 waves (boss) for now
 func _regenerate_new_map_layout():
 	$UI/ReloadSceneText.visible = true
 	$UI/MarginContainer/HBoxContainer/NextWaveButton.visible = false
 	TransitionLayer.reload_level("res://Scenes/Maps/easy_map.tscn")
 
-
+# Placeholder implementation of map regeneration
 func _on_no_enemies_left_on_map():
 	next_wave_button.visible = true
 	if Globals.easy_map_current_level % 5 == 0:
--- Scripts/map_parent.gd
+++ Scripts/map_parent.gd
@@ -10,46 +10,26 @@ class_name MapParent
 @export var tile_crossroads:PackedScene
 @export var tile_empty:Array[PackedScene]
 
-@export var map_length:int = 16
-@export var map_height:int = 10
-
-@export var min_path_size: int = 50
-@export var max_path_size: int = 70
-@export var min_loops: int = 3
-@export var max_loops: int = 5
-
-@onready var _path_generator = PathGenerator.new(map_length, map_height)
-
 @onready var enemies_on_map: int = 0
 var temp_enemy_size: int
 
 signal no_enemies_left_on_map
 
 func _ready():
-	print("Parent loaded map")
+	_complete_grid()
 
 func _complete_grid():
-	for x in range(map_length):
-		for y in range(map_height):
-			if not _path_generator.get_path().has(Vector2i(x,y)):
+	for x in range(PathGenInstance.path_config.map_length):
+		for y in range(PathGenInstance.path_config.map_height):
+			if not PathGenInstance.get_path_route().has(Vector2i(x,y)):
 				var tile:Node3D = tile_empty.pick_random().instantiate()
 				add_child(tile)
 				tile.global_position = Vector3(x, 0, y)
 				tile.global_rotation_degrees = Vector3(0, randi_range(0,3)*90, 0)
 	
-func _display_path():
-	var iteration_count:int = 1
-	_path_generator.generate_path(true)
-
-	while _path_generator.get_path().size() < min_path_size or _path_generator.get_path().size() > max_path_size or _path_generator.get_loop_count() < min_loops or _path_generator.get_loop_count() > max_loops:
-		iteration_count += 1
-		_path_generator.generate_path(true)
-
-	print("Generated a path of %d tiles after %d iterations" % [_path_generator.get_path().size(), iteration_count])
-	print(_path_generator.get_path())
 	
-	for i in range(_path_generator.get_path().size()):
-		var tile_score:int = _path_generator.get_tile_score(i)
+	for i in range(PathGenInstance.get_path_route().size()):
+		var tile_score:int = PathGenInstance.get_tile_score(i)
 		
 		var tile:Node3D = tile_empty[0].instantiate()
 		var tile_rotation: Vector3 = Vector3.ZERO
@@ -83,12 +63,8 @@ func _display_path():
 			tile_rotation = Vector3(0,0,0)
 			
 		add_child(tile)
-		tile.global_position = Vector3(_path_generator.get_path_tile(i).x, 0, _path_generator.get_path_tile(i).y)
+		tile.global_position = Vector3(PathGenInstance.get_path_tile(i).x, 0, PathGenInstance.get_path_tile(i).y)
 		tile.global_rotation_degrees = tile_rotation
-		
-func _add_curve_point(c3d:Curve3D, v3:Vector3) ->bool:
-	c3d.add_point(v3)
-	return true
 	
 func _spawn_enemy(chosen_enemy: PackedScene):
 	enemies_on_map += 1
@@ -96,7 +72,7 @@ func _spawn_enemy(chosen_enemy: PackedScene):
 	
 	var c3d:Curve3D = Curve3D.new()
 	
-	for element in _path_generator.get_path():
+	for element in PathGenInstance.get_path_route():
 		c3d.add_point(Vector3(element.x, 0.4, element.y))
 
 	var p3d:Path3D = Path3D.new()
