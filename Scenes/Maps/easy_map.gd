3f66947b56ae34e1af9b35749c339e857912bc1b
james <75051061+notsceptor@users.noreply.github.com>
2024-03-25 23:59:13 +0000

    Implemented raycasting
    Necessary for future implementation of turrets to avoid colliding with unwanted obstacles

--- Scenes/Maps/easy_map.gd
+++ Scenes/Maps/easy_map.gd
@@ -3,9 +3,26 @@ extends MapParent
 var enemy_array: Array = ["Scumbug", "Giant Zombie Snail"]
 @onready var next_wave_button: Button = $UI/MarginContainer/HBoxContainer/NextWaveButton
 
+@onready var cam = $Camera3D
+var RAYCAST_LENGTH:float = 100
+
 func _ready():
 	_display_path()
 	_complete_grid()
+	
+func _physics_process(_delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		var space_state = get_world_3d().direct_space_state
+		var mouse_pos:Vector2 = get_viewport().get_mouse_position()
+		var origin:Vector3 = cam.project_ray_origin(mouse_pos)
+		var end:Vector3 = origin + cam.project_ray_normal(mouse_pos) * RAYCAST_LENGTH
+		var query = PhysicsRayQueryParameters3D.create(origin, end)
+		query.collide_with_areas = true
+		var rayResult:Dictionary = space_state.intersect_ray(query)
+		if rayResult.size() > 0:
+			var co:CollisionObject3D = rayResult.get("collider")
+			print(co.get_groups())
+		
 	
 func _on_ui_next_wave_button_pressed(wave_number, wave_size):
 	Globals.wave_ongoing = true
