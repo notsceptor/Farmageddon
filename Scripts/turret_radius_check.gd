20163327abf3329f5ee403f2e4b20a2888a31916
Trezeno <113196678+Trezeno@users.noreply.github.com>
2024-04-04 21:49:04 +0100

    Potato Launcher implemented

--- Scripts/turret_radius_check.gd
+++ Scripts/turret_radius_check.gd
@@ -8,12 +8,3 @@ func _on_area_radius_area_entered(area):
 
 func _on_area_radius_area_exited(area):
 	area_exited.emit(area)
-
-
-
-func _on_area_entered(area):
-	pass # Replace with function body.
-
-
-func _on_area_exited(area):
-	pass # Replace with function body.
