f813e3599353afce7f48c4b14499b9005031a6ac
Trezeno <113196678+Trezeno@users.noreply.github.com>
2024-04-07 11:39:00 +0100

    Basic health bar implemented
    - Health bar scene created
    - Health bar added to Scumbug and Snail

--- Scenes/Enemies/Scumbug/scumbug.gd
+++ Scenes/Enemies/Scumbug/scumbug.gd
@@ -4,12 +4,15 @@ var _path_progress: float = 0.0
 
 @onready var _path_follow_3d: PathFollow3D
 
+@onready var health_bar = $SubViewport/HealthBar3D
+
 signal died
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_name = "Scumbug"
 	_health = 10
+	health_bar.max_value = _health
 	_size = 1
 	Globals.temp_enemy_size = _size
 	_speed = 3
@@ -21,5 +24,6 @@ func _on_moving_state_processing(delta):
 
 func _on_area_3d_area_entered(area):
 	_health -= area.damage
+	health_bar.value -= area.damage
 	if _health <= 0:
 		get_node("../../../").queue_free() # Fetches root node (container) and removes it
