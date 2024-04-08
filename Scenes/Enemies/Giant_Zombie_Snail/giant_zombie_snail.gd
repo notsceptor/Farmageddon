d4391c95404026da74ae597c113bd556269269d6
Trezeno <113196678+Trezeno@users.noreply.github.com>
2024-04-08 10:32:35 +0100

    Spore Sprayer implementation
    - Tweaked AOE damage timer (up 0.25s -> 0.75s) to better help balance AOE turrets damage
    - Set spore damage to 1 and chiligun damage to 2 for now
    - Altered the Spore Sprayer model to have the full 360 degree AOE model
    - Altered the snail script to also check for AOE group of projectiles

--- Scenes/Enemies/Giant_Zombie_Snail/giant_zombie_snail.gd
+++ Scenes/Enemies/Giant_Zombie_Snail/giant_zombie_snail.gd
@@ -27,7 +27,7 @@ func _on_moving_state_processing(delta):
 	_path_follow_3d.progress = _path_progress
 
 func _on_area_3d_area_entered(area):
-	if area.projectile_type == "aoe":
+	if area.is_in_group("AOE"):
 		in_constant_aoe_damage_zone = true
 		area_damage_to_take = area.damage
 	else:
@@ -38,7 +38,7 @@ func _on_area_3d_area_entered(area):
 				get_node("../../../").queue_free() # Fetches root node (container) and removes it
 
 func _on_area_3d_area_exited(area):
-	if area.projectile_type == "aoe":
+	if area.is_in_group("AOE"):
 		in_constant_aoe_damage_zone = false
 		area_damage_to_take = 0
 
--- Scenes/Turrets/Spore Sprayer/spore_spray_projectile.gd
+++ Scenes/Turrets/Spore Sprayer/spore_spray_projectile.gd
@@ -1,19 +1,17 @@
 extends Area3D
 
-@onready var damage: int = 2
+@onready var damage: int = 1
 
 var enemy_list: Array[Area3D]
 
 func _process(_delta):
 	print(enemy_list)
 	if enemy_list.size() > 0:
-		$ChiliSpray.emitting = true
-		$ChiliSpray2.emitting = true
-		$ChiliSpray3.emitting = true
+		$SporeSpray.emitting = true
+		$SporeSpray2.emitting = true
 	else:
-		$ChiliSpray.emitting = false
-		$ChiliSpray2.emitting = false
-		$ChiliSpray3.emitting = false
+		$SporeSpray.emitting = false
+		$SporeSpray2.emitting = false
 
 func _on_area_entered(area):
 	enemy_list.append(area)
