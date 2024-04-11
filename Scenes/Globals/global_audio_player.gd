e1404b8e1e8223f74230b6a13363f32f57d21e2f
Axebow2 <149180749+Axebow2@users.noreply.github.com>
2024-04-11 20:43:12 +0100

    Audio Changes
    Added sound effect for fail state and earthquake, started on fixing the placement sound.

--- Scenes/Globals/global_audio_player.gd
+++ Scenes/Globals/global_audio_player.gd
@@ -1,8 +1,18 @@
 extends Node
 
 func play_snail_death_sound():
+	$SnailDeathSound.pitch_scale = randf_range(0.5, 0.7)
 	$SnailDeathSound.play()
 
 func play_scumbug_death_sound():
 	$ScumbugDeathSound.pitch_scale = randf_range(1.1, 1.3)
 	$ScumbugDeathSound.play()
+	
+func play_earthquake_sound():
+	$EarthquakeSound.play()
+
+func play_fail_sound():
+	$FailSound.play()
+	
+func play_placement_sound():
+	$Placement.play()
