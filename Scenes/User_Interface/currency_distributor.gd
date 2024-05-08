9b75eedbf32084d89427bf795ff85b31759321fe
Wilhaz <wilsontharry@gmail.com>
2024-05-08 21:29:48 +0100

    Removed Coroutine Ticking
    Removed the Coroutine Ticking to just be a linear addition rather than ticking up, as it messes with joint enemy deaths.

--- Scenes/User_Interface/currency_distributor.gd
+++ Scenes/User_Interface/currency_distributor.gd
@@ -8,46 +8,47 @@ signal gems_updated
 signal scrap_updated
 
 func addGold(amount):
-	start_coroutine(int(amount), "gold")
+	Globals.gold += amount
 
 func subtractGold(amount):
 	if amount > Globals.gold:
 		amount = Globals.gold
-	start_coroutine(int(-amount), "gold")
+	Globals.gold -= amount
 
 func addScrapMetal(amount):
-	start_coroutine(int(amount), "scrap")
+	Globals.scrap += amount
 
 func subtractScrapMetal(amount):
 	if amount > Globals.scrap:
 		amount = Globals.scrap
-	start_coroutine(int(-amount), "scrap")
+	Globals.scrap -= amount
 
 func addGems(amount):
-	start_coroutine(int(amount), "gems")
+	Globals.gems += amount
 
 func subtractGems(amount):
 	if amount > Globals.gems:
 		amount = Globals.gems
-	start_coroutine(int(-amount), "gems")
+	Globals.gems -= amount
 
-func start_coroutine(amount, resource_name):
-	match resource_name:
-		"gold":
-			await update_resource(amount, "gold")
-		"scrap":
-			await update_resource(amount, "scrap")
-		"gems":
-			await update_resource(amount, "gems")
 
-func update_resource(amount, resource_name):
-	var start_value = Globals.get(resource_name)
-	var target_value = start_value + amount
-	var elapsed_time = 0.0
-	
-	while elapsed_time < update_duration:
-		elapsed_time += get_process_delta_time()
-		Globals.set(resource_name, round(lerp(start_value, target_value, elapsed_time / update_duration)))
-		await get_tree().process_frame
-	
-	Globals.set(resource_name, target_value)
+#func start_coroutine(amount, resource_name):
+	#match resource_name:
+		#"gold":
+			#await update_resource(amount, "gold")
+		#"scrap":
+			#await update_resource(amount, "scrap")
+		#"gems":
+			#await update_resource(amount, "gems")
+#
+#func update_resource(amount, resource_name):
+	#var start_value = Globals.get(resource_name)
+	#var target_value = start_value + amount
+	#var elapsed_time = 0.0
+	#
+	#while elapsed_time < update_duration:
+		#elapsed_time += get_process_delta_time()
+		#Globals.set(resource_name, round(lerp(start_value, target_value, elapsed_time / update_duration)))
+		#await get_tree().process_frame
+	#
+	#Globals.set(resource_name, target_value)
