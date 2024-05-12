c966cb37e28a9373d1f41c93181a44ab590d0922
Trezeno <113196678+Trezeno@users.noreply.github.com>
2024-05-12 12:00:29 +0100

    Settings menu now correctly changes audio

--- Scenes/User_Interface/settings_screen.gd
+++ Scenes/User_Interface/settings_screen.gd
@@ -7,7 +7,3 @@ signal back_button_pressed()
 func _on_back_button_pressed():
 	print("BACK")
 	back_button_pressed.emit()
-
-func _on_music_vol_slider_value_changed(value):
-	music_volume_text.text = str(value)
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
