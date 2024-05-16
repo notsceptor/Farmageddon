b2386a08e30f34cd0a9e4690aaec62f49d061ed8
Trezeno <113196678+Trezeno@users.noreply.github.com>
2024-05-16 11:10:51 +0100

    Added SaveLoad script

---
+++ Scripts/save_and_load.gd
@@ -0,0 +1,40 @@
+extends Node
+
+const SAVE_PATH = "user://gamedata.save"
+
+func save_game() -> void:
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	file.store_var(Inventory.items)
+	file.store_var(Globals.easy_map_current_level)
+	file.store_var(Globals.easy_map_spawn_size)
+	file.store_var(Globals.medium_map_current_level)
+	file.store_var(Globals.medium_map_spawn_size)
+	file.store_var(Globals.hard_map_current_level)
+	file.store_var(Globals.hard_map_spawn_size)
+	file.store_var(Globals.current_max_turrets)
+	file.store_var(Globals.master_volume_value)
+	file.store_var(Globals.music_volume_value)
+	file.store_var(Globals.sfx_volume_value)
+	file.store_var(Globals.gold)
+	file.store_var(Globals.scrap)
+	file.store_var(Globals.gems)
+	
+func load_game() -> void:
+	if FileAccess.file_exists(SAVE_PATH):
+		var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+		Inventory.items = file.get_var()
+		Globals.easy_map_current_level = file.get_var()
+		Globals.easy_map_spawn_size = file.get_var()
+		Globals.medium_map_current_level = file.get_var()
+		Globals.medium_map_spawn_size = file.get_var()
+		Globals.hard_map_current_level = file.get_var()
+		Globals.hard_map_spawn_size = file.get_var()
+		Globals.current_max_turrets = file.get_var()
+		Globals.master_volume_value = file.get_var()
+		Globals.music_volume_value = file.get_var()
+		Globals.sfx_volume_value = file.get_var()
+		Globals.gold = file.get_var()
+		Globals.scrap = file.get_var()
+		Globals.gems = file.get_var()
+	else:
+		print("File doesn't yet exist. Continue as normal")
